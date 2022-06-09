98458de4d19c303592e16ef32e5a3cfc45f97a44
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-06-09 10:50:55 +0200

    Added form validation and card on-hover effect

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -9,8 +9,12 @@ export default function Login() {
 			</a>
 			<div className="login-card">
 				<p className="login-title">Login</p>
-				<input className="login-input" type="email" placeholder="Email address" name="email" required />
-				<input className="login-input" type="password" placeholder="Password" name="password" required />
+				<div>
+					<input className="login-input" type="email" placeholder="Email address" name="email" required />
+				</div>
+				<div>
+					<input className="login-input" type="password" placeholder="Password" name="password" required />
+				</div>
 				<div className="clearfix">
 					<button className="submit-btn" type="submit">
 						Login to your account
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,16 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.svg";
 export default function SignUp() {
+	const initialValues = { email: "", password: "", repassword: "" };
+	const [formValues, setFormValues] = useState(initialValues);
+	const [formErrors, setFormErrors] = useState({});
+	const [isSubmit, setIsSubmit] = useState(false);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormValues({ ...formValues, [name]: value });
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setFormErrors(validate(formValues));
+		setIsSubmit(true);
+	};
+	useEffect(() => {
+		if (Object.keys(formErrors).length === 0 && isSubmit) {
+			console.log(formValues);
+		}
+	}, [formErrors]);
+	const validate = (values) => {
+		const errors = {};
+		const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+		if (!values.email) {
+			errors.email = "Required";
+		} else if (!regex.test(values.email)) {
+			errors.email = "Invalid";
+		}
+		if (!values.password) {
+			errors.password = "Required";
+		} else if (values.password.length < 6) {
+			errors.password = "At least 6 characters";
+		} else if (values.password.length > 12) {
+			errors.password = "Cannot exceed 12 characters";
+		}
+		if (values.repassword !== values.password) {
+			errors.repassword = "Must match";
+		}
+		return errors;
+	};
+
 	return (
-		<form action="" className="login signup">
+		<form onSubmit={handleSubmit} className="login signup">
 			<a href="/">
 				<img className="login-logo" src={logo} alt="Logo" />
 			</a>
 			<div className="login-card">
 				<p className="login-title">Sign Up</p>
-				<input className="login-input" type="email" placeholder="Email address" name="email" required />
-				<input className="login-input" type="password" placeholder="Password" name="psw" required />
-				<input className="login-input" type="password" placeholder="Repeat Password" name="psw-repeat" required />
+				<div className="field">
+					<input
+						className="login-input"
+						type="email"
+						placeholder="Email address"
+						name="email"
+						value={formValues.email}
+						onChange={handleChange}
+					/>
+					<div className="error-message">{formErrors.email}</div>
+				</div>
+				<div className="field">
+					<input
+						className="login-input"
+						type="password"
+						placeholder="Password"
+						name="password"
+						value={formValues.password}
+						onChange={handleChange}
+					/>
+					<div className="error-message">{formErrors.password}</div>
+				</div>
+				<div className="field">
+					<input
+						className="login-input"
+						type="password"
+						placeholder="Repeat Password"
+						name="repassword"
+						value={formValues.repassword}
+						onChange={handleChange}
+					/>
+					<div className="error-message">{formErrors.repassword}</div>
+				</div>
 				<div className="clearfix">
 					<button className="submit-btn" type="submit">
 						Create an account
